35a9b3b0d3717e0cbb180aa47a9bdc333ce0d64c
charleshall1998 <charleskhanhhall@gmail.com>
2023-12-05 20:12:11 -0500

    Add project files.

---
+++ TicTacToe/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, what is your name?");
+
+var name = Console.ReadLine();
+
+Console.WriteLine($"Hello, {name}. Let's play some Tic Tac Toe!");
